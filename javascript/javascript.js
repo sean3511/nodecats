d92557ab77c76eafbafc134624030a99ae089be0
Sloly <yahoo3511@gmail.com>
2024-03-05 01:01:36 +0800

    跳出通知也加上去，確保沒問題

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,14 +6,19 @@
 
     // 複製功能
     $(document).ready(function() {
-        $('.copybtn').click(function() {
+      $('.copybtn').click(function() {
           // 使用原生 JavaScript 或 jQuery 來選擇並複製文字
           var textToCopy = document.getElementById('textToCopy').innerText;
           navigator.clipboard.writeText(textToCopy).then(function() {
-            // 複製成功後，使用 jQuery 來顯示 popup_text
-            $('.popup_text').fadeIn().delay(1500).fadeOut();
+              // 複製成功後，使用 jQuery 來顯示 popup_text
+              $('.popup_text').fadeIn().delay(1500).fadeOut();
+  
+              // 顯示一個警告框通知用戶複製成功
+              alert('Copy successful!');
           }).catch(function(error) {
-            console.error('複製失敗', error);
+              // 如果複製過程中出現錯誤，則在控制台顯示錯誤信息
+              console.error('Copy failed', error);
           });
-        });
       });
+  });
+  
